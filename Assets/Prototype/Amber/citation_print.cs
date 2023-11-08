610daa2b32ad548203c149c42b8e7c9cf81e7f8e
LAPTOP-SQ03A8DH\Amber <Amberqu.q2@gmail.com>
2023-11-08 16:34:42 -0500

    amber prototype

---
+++ Assets/Prototype/Amber/citation_print.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class citation_print : MonoBehaviour
+{
+    public Transform Printer;
+
+    private Vector3 endPosition = new Vector3(5, 4, 0);
+    private Vector3 startPosition = Printer.position;
+    private float duration = 3f;
+    private float elapsedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        float percentComplete = elapsedTime / duration;
+
+        transform.position = Vector3.Lerp(startPosition, endPosition, percentComplete);
+    }
+}
