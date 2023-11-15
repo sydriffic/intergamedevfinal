831847977f7ff9bef7d586245fbf2e3c4ed5fdb1
LeoGenius233 <kf2380@nyu.edu>
2023-11-15 13:17:49 -0500

    Dialogue system prototype

---
+++ Assets/Prototype/Leo/DialogueTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Message[] messages;
+    public Actor[] actors;
+
+    public void StartDialogue()
+    {
+       // FindObjectOfType<DialogueManager>().OpenDialogue(messages, actors);
+    }
+
+
+}
+
+
+[System.Serializable]
+public class Message
+{
+    public int actorID;
+    public string messages;
+}
+
+[System.Serializable]
+public class Actor
+{
+    public string name;
+    public Sprite sprite;
+}
