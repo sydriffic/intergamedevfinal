4fc61d36f636a84b88af71b1b2a09fe19424eff6
LAPTOP-SQ03A8DH\Amber <Amberqu.q2@gmail.com>
2023-12-07 16:50:46 -0500

    updated typewriter effect, main menu, and intro

--- Assets/Scripts/typewriter.cs
+++ Assets/Scripts/typewriter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,83 +6,167 @@ using TMPro;
 
 public class typewriter : MonoBehaviour
 {
-	Text _text;
-	TMP_Text _tmpProText;
+	[SerializeField] Text text;
+	[SerializeField] TMP_Text tmpProText;
 	string writer;
+	[SerializeField] private Coroutine coroutine;
 
 	[SerializeField] float delayBeforeStart = 0f;
 	[SerializeField] float timeBtwChars = 0.1f;
 	[SerializeField] string leadingChar = "";
 	[SerializeField] bool leadingCharBeforeDelay = false;
+	[Space(10)] [SerializeField] private bool startOnEnable = false;
+
+	[Header("Collision-Based")]
+	[SerializeField] private bool clearAtStart = false;
+	[SerializeField] private bool startOnCollision = false;
+	enum options { clear, complete }
+	[SerializeField] options collisionExitOptions;
 
 	// Use this for initialization
+	void Awake()
+	{
+		if (text != null)
+		{
+			writer = text.text;
+		}
+
+		if (tmpProText != null)
+		{
+			writer = tmpProText.text;
+		}
+	}
+
 	void Start()
 	{
-		_text = GetComponent<Text>()!;
-		_tmpProText = GetComponent<TMP_Text>()!;
-		
-
-
-		if (_text != null)
+		if (!clearAtStart) return;
+		if (text != null)
 		{
-			writer = _text.text;
-			_text.text = "";
+			text.text = "";
+		}
+
+		if (tmpProText != null)
+		{
+			tmpProText.text = "";
+		}
+	}
+
+	private void OnEnable()
+	{
+		print("On Enable!");
+		if (startOnEnable) StartTypewriter();
+	}
+
+	private void OnCollisionEnter2D(Collision2D col)
+	{
+		print("Collision!");
+		if (startOnCollision)
+		{
+			StartTypewriter();
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D other)
+	{
+		if (collisionExitOptions == options.complete)
+		{
+			if (text != null)
+			{
+				text.text = writer;
+			}
+
+			if (tmpProText != null)
+			{
+				tmpProText.text = writer;
+			}
+		}
+		// clear
+		else
+		{
+			if (text != null)
+			{
+				text.text = "";
+			}
+
+			if (tmpProText != null)
+			{
+				tmpProText.text = "";
+			}
+		}
+
+		StopAllCoroutines();
+	}
+
+
+	private void StartTypewriter()
+	{
+		StopAllCoroutines();
+
+		if (text != null)
+		{
+			text.text = "";
 
 			StartCoroutine("TypeWriterText");
 		}
-		
-		if (_tmpProText != null)
+
+		if (tmpProText != null)
 		{
-			writer = _tmpProText.text;
-			_tmpProText.text = "";
+			tmpProText.text = "";
 
 			StartCoroutine("TypeWriterTMP");
 		}
 	}
 
+	private void OnDisable()
+	{
+		StopAllCoroutines();
+	}
+
 	IEnumerator TypeWriterText()
 	{
-		_text.text = leadingCharBeforeDelay ? leadingChar : "";
+		text.text = leadingCharBeforeDelay ? leadingChar : "";
 
 		yield return new WaitForSeconds(delayBeforeStart);
 
 		foreach (char c in writer)
 		{
-			if (_text.text.Length > 0)
+			if (text.text.Length > 0)
 			{
-				_text.text = _text.text.Substring(0, _text.text.Length - leadingChar.Length);
+				text.text = text.text.Substring(0, text.text.Length - leadingChar.Length);
 			}
-			_text.text += c;
-			_text.text += leadingChar;
+			text.text += c;
+			text.text += leadingChar;
 			yield return new WaitForSeconds(timeBtwChars);
 		}
 
 		if (leadingChar != "")
 		{
-			_text.text = _text.text.Substring(0, _text.text.Length - leadingChar.Length);
+			text.text = text.text.Substring(0, text.text.Length - leadingChar.Length);
 		}
+
+		yield return null;
 	}
 
 	IEnumerator TypeWriterTMP()
 	{
-		_tmpProText.text = leadingCharBeforeDelay ? leadingChar : "";
+		tmpProText.text = leadingCharBeforeDelay ? leadingChar : "";
 
 		yield return new WaitForSeconds(delayBeforeStart);
 
 		foreach (char c in writer)
 		{
-			if (_tmpProText.text.Length > 0)
+			if (tmpProText.text.Length > 0)
 			{
-				_tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
+				tmpProText.text = tmpProText.text.Substring(0, tmpProText.text.Length - leadingChar.Length);
 			}
-			_tmpProText.text += c;
-			_tmpProText.text += leadingChar;
+			tmpProText.text += c;
+			tmpProText.text += leadingChar;
 			yield return new WaitForSeconds(timeBtwChars);
 		}
 
 		if (leadingChar != "")
 		{
-			_tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
+			tmpProText.text = tmpProText.text.Substring(0, tmpProText.text.Length - leadingChar.Length);
 		}
 	}
 }
